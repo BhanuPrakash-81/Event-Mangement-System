f2297f1cc90efe82c30080121409119bc8a4477c
BhanuPrakash-81 <chimmilibhanuprakash313@gmail.com>
2026-04-24 22:10:10 +0530

    Cleanup: Remove legacy root project files

--- src/main/java/com/event_management/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.event_management.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.event_management.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
